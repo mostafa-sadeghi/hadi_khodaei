420de73c42213db4c002f55125d27f2d43f3aa23
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-25 21:58:22 +0330

    sadeghi

---
+++ s2.py
@@ -0,0 +1,72 @@
+# "#" means commment
+# python operators
+# symbol        Operation
+# +             Add
+# -             Sub
+# *             Mul
+# /             Div
+
+# print(5 + 30 * 20)
+# print((5 + 30) * 20)
+# x = 10
+# print(x)
+# print(type(x))
+
+# x = 'python'
+# print(x)
+# print(type(x))
+
+# how to naming variables in python
+# هر ترکیبی از کاراکترهای الفبایی انگلیسی و اعداد و آندرلاین به استثنای اینکه اولین
+# کاراکتر نمی تواند عدد باشد
+
+# number1 = 1
+# number_1 = 1
+# _number1 = 2
+# 1number = 1 # error
+# number 1 = 12 # error
+# number-1 = 1 #error
+
+# number_one = 12 # snake case
+# numberone = 12 #  not recommended
+# numberOne = 12 # camelCase
+
+# primitive types in python
+# numbers       str         bool
+# number = 1
+# print(type(number)) # int 
+# number = 1.0
+# print(type(number)) # float
+# number = 1 + 2j
+# print(type(number)) # complex
+
+# message_one = ''
+# print(type(message_one))
+# message_two = ""
+# print(type(message_two))
+
+# name = "nikan"
+# print(len(name))
+# print(type(name))
+# message = """hello everybody. welcome to our python class. 
+# we are going to learn everythings about python from zero to mastery"""
+# print(message)
+
+# message = '''He said, "You couldn't do that"'''
+# print(message)
+# message = 'He said, "You can do that"'
+# print(message)
+# message = 'He said, "You couldn\'t do that"'
+# print(message)
+
+# exercise 1 :
+# print this statement:
+# He said, "Aren't can't shouldn't wouldn't."
+
+# myscore = 1000
+# message = 'My score is %s'%myscore
+# print(message)
+name = "sara"
+myscore = 1000
+message = '%s your score is %s'%(name,myscore)
+print(message)
