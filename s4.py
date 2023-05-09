0b12b0aa5d5c17a7e08cb736e313d47d5a9645ae
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-09 21:56:26 +0330

    sadeghi

---
+++ s4.py
@@ -0,0 +1,158 @@
+# favorite_sports = ["football", "basketball", "tennis", "pingpong"]
+# favorite_sports = {}
+# print(type(favorite_sports))
+
+# favorite_sports['reza'] = "football"
+# favorite_sports['tina'] = "basketball"
+# favorite_sports['ali'] = "football"
+# favorite_sports['milad'] = "tennis"
+
+# print(favorite_sports)
+# all_persons = []
+# name = input('enter your name: ')
+# sport = input('enter your favorite sport: ')
+# favorite_sports = {}
+# favorite_sports[name] = sport
+
+# all_persons.append(favorite_sports)
+
+# name = input('enter your name: ')
+# sport = input('enter your favorite sport: ')
+# favorite_sports = {}
+# favorite_sports[name] = sport
+
+# all_persons.append(favorite_sports)
+# print(all_persons)
+# print(all_persons[0])
+# print(all_persons[0]['tina'])
+# del all_persons[0]
+
+# favorite_sports = {}
+# print(type(favorite_sports))
+
+# favorite_sports['reza'] = "football"
+# favorite_sports['tina'] = "basketball"
+# favorite_sports['ali'] = "football"
+# favorite_sports['milad'] = "tennis"
+
+# print(favorite_sports)
+# del favorite_sports['ali']
+# print(favorite_sports)
+
+
+# import turtle
+
+# screen = turtle.Screen()
+# screen.register_shape('strawberry.gif')
+
+# screen.bgcolor('orange')
+# # screen.bgpic('mario.png')
+# pen = turtle.Turtle()
+# # 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'
+# pen.shape('circle')
+# # pen.shape('strawberry.gif')
+# # pen.shapesize(3)
+# pen.color("red", "green")
+# pen.pensize(4)
+
+# pen.stamp()
+# pen.forward(100)
+# pen.left(120)
+
+# pen.stamp()
+# pen.forward(100)
+# pen.left(120)
+
+# pen.stamp()
+# pen.forward(100)
+# pen.left(120)
+
+# turtle.done()
+
+# برنامه ای بنویس که مربع و پنج ضلعی بکشد
+
+# print("2 + 3")
+# print(2 + 3)
+
+# print("Hello world")
+
+
+# print("the file is stored in c:\\newfolder")
+
+# print("Hello\rHi")
+# print("Hello every body.\rwelcome to python class.\rwelcome to pygame")
+
+
+# x = 1
+
+# x = x + 1
+
+# print(x)
+
+# print(5//2)
+
+# print(5 ** 2)
+# print(5 % 2)
+
+
+# برنامه ای بنویس که نام دانش آموزی را از ورودی دریاتف نماید و 
+# نمرات سه درس وی را بگیرد 
+# در نهایت معدل دانش آموزا را محاسبه نماید و به همراه اسم وی نمایش دهد
+
+
+# a = 40
+# b = 5
+
+# if a < b :
+#     print("a is less than b")
+#     print("ok")
+
+# if a > b:
+#     print("a is greater than b")
+
+# if a >= b:
+#     print("a is greater than or equal to b")
+
+# if a <= b:
+#     print("a is less than or equal to b")
+
+# if a == b:
+#     print("a and b are equal")
+# if a != b:
+#     print("a and b are not equal")
+    # print()
+
+# برنامه ای بنویس که یک عدد را از ورودی دریافت نماید و در صورت زوج بودن
+# even
+# و در صورت فرد بودن
+# odd
+# را پرینت نماید
+
+# a = 4
+# b = 4
+
+# if a >= b :
+#     print("a is greater than or equal to b")
+
+# if a > b or a == b:
+#     print("a is greater than or equal to b")
+
+# a = 3
+# b = 4
+# c = 2
+
+# if a > b and a > c :
+#     print("a is greater than b and c")
+
+# if a > b or a > c:
+#     print("a is greater tha nb or c")
+
+# boolean    True False
+
+# a = True
+# if a:
+#     print("balalalallala")
+
+b = False
+if not b:
+    print("cccccccccccccccccc")
