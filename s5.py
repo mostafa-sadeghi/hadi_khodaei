4f21e66bcc80e9470a2a8db092b453a05f784705
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-16 18:59:32 +0330

    sadeghi

---
+++ s5.py
@@ -0,0 +1,134 @@
+# a = True
+# b = False
+
+
+# if a and b:
+#     print("a and b are both true")
+
+# if a or b:
+#     print("a or b is true")
+
+# a = 3
+# b = 3
+# c = a == b
+# print(c)
+
+# Truethy anf falsy
+# if 1:
+#     print("1")
+
+# if -1:
+#     print("-1")
+# if 0:
+#     print("0")
+
+# if "a":
+#     print("a")
+# if " ":
+#     print("nothing")
+
+# a = "c"
+# if a == "B" or "b":
+#     print("a is equal to b. Maybe")
+
+# if a == "B" or a == "b":
+#     print("a is equal to b.")
+
+# user_name = input('enter a user_name ')
+# if user_name == 'admin':
+#     print("login success")
+#     print("user is valid")
+
+# برنامه ای بنویس که نام کاربری و کلمه عبور را از کاربر دریافت نماید و در صورت مجاز بودن
+# پیغام مناسبی را پرینت نماید
+# در صورت غیر مجاز بودن نیز پیغام مناسبی را پرینت نماید
+# نام های کاربری مجاز:
+# admin     root
+
+# کلمه عبور مجاز
+# root
+
+
+# print("hi")
+# >>> numbers = range(10)
+# >>> numbers
+# range(0, 10)
+# >>> type(numbers)
+# <class 'range'>
+# >>> numbers[0]
+# 0
+# >>> numbers[1]
+# 1
+# >>> numbers[9]
+# 9
+# >>> numbers[10]
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# IndexError: range object index out of range
+# >>> numbers = range(3,50)
+# >>> numbers[0]
+# 3
+# >>> len(numbers)
+# 47
+# >>>   
+
+# for i in range(10):
+#     # print(i, end=' ')
+#     print("python".upper(), end=" ")
+#     print("blalalal")
+
+# print("outside")
+
+# for number in range(1,11):
+#     print(number)
+
+# با کمک ترتل و حلقه فور اشکال زیر را ترسیم نمائید
+# مثلث
+# مربع
+# پنجضلعی
+# exercise :
+'''
+با کمک حلقه برنامه ای بنویس که پنج عدد از ورودی دریافت نماید و حاصل جمع آنها را
+محاسبه و نمایش دهد
+در برنامه بالا فقط حاصل جمع اعداد فرد را نمایش بده
+در برنامه بالا فقط حاصل جمع اعداد زوج را نمایش بده
+'''
+
+# for x in range(2,11,2):
+#     print(x)
+
+# exer:
+'''
+برنامه ای بنویس که مجموع اعداد زوج بین ده هزار تا سی هزار را محاسبه و نمایش دهد
+برنامه ای بنویس که مجموع اعداد فرد بین ده هزار تا سی هزار را محاسبه و نمایش دهد
+'''
+
+# for i in range(10,0,-2):
+#     print(i)
+
+# exer: برنامه ای بنویس که نام کاربر را از ورودی دریافت نماید و کاراکترهای آن را از انتها به ابتدا نمایش دهد
+# for s in 'python':
+#     print(s)
+
+# for i in [2,5,8,9,100]:
+#     print(i)
+
+# for i in range(3):
+#     print("a")
+# for j in range(3):
+#     print("b")
+
+# for i in range(3):
+#     print("a")
+#     for j in range(3):
+#         print("b")
+# total = 0
+# for i in range(5):
+#     new_number = float(input('enter a number: '))
+#     if new_number == 100:
+#         total = total + 1 # total += 1
+
+# print("total numbers of 100:",total)
+
+
+# https://app.diagrams.net/
